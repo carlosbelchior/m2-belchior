5dae4cb8dadd324792e1b40e9f93b89f4b2f912e
Carlos Belchior <carlosbelchiorsilva@gmail.com>
2022-06-17 11:11:40 -0300

    Models

---
+++ app/Models/CitiesGroup.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CitiesGroup extends Model
+{
+    use HasFactory;
+    protected $table = 'cities_group';
+}
