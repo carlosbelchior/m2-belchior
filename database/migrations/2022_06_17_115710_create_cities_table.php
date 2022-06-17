0456c54287e75d4f9f0553887183d82b3db9eccc
Carlos Belchior <carlosbelchiorsilva@gmail.com>
2022-06-17 11:06:28 -0300

    Migrations

--- database/migrations/2022_06_17_115710_create_cities_table.php
+++ database/migrations/2022_06_17_115710_create_cities_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('state', 2);
+            $table->foreignId('group_id')->constrained('cities_group');
             $table->timestamps();
+
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cidades');
     }
 };
--- database/migrations/2022_06_17_115855_create_campaigns_table.php
+++ database/migrations/2022_06_17_115855_create_campaigns_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('groupd_id')->constrained('cities_group');
+            $table->string('name');
+            $table->date('validity')->nullable();
+            $table->integer('status');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('campaigns');
     }
 };
